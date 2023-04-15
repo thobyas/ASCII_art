9417573b1f371f53371294e25d332bc3789e19f5
thobyas <thobyascastano@gmail.com>
2023-04-14 23:28:50 -0500

    Problemas en la creacion de la matriz con filtro promedio

--- load_image.py
+++ load_image.py
@@ -6,8 +6,31 @@ def img_shape(pc_path):
     img = cv2.imread(pc_path,0) #Leo la imagen en escala de grises con el paremetro en "0"
 #DISPLAY
     resolution = img.shape
+    #cv2.imshow('image',img)
+    #cv2.waitKey(0)
     return resolution
 
 def img_pixel(pc_path):
     img = cv2.imread(pc_path) #Leo la imagen
-    print(img)
+    #print(img)
+    return img
+
+def filter(type, pixel,size):  
+    filter_img = []
+    img_out = []
+    if type == "average":
+        for i in range(2):
+            for j in range(1):
+                #aver_value = (int(pixel[i,j][0]) + int(pixel[i,j][1]) + int(pixel[i,j][2]))/3
+                aver_value = 3
+                filter_img.append([aver_value, aver_value, aver_value])
+            img_out.append(filter_img)
+        else:
+            pass
+            #cv2.imshow('image',pixel)
+            #cv2.waitKey(0)
+    # cv2.imshow('hola',img_out)
+    # cv2.waitKey(0)
+    print(img_out)
+
+
--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import load_image
 
 def main():
     resolution = load_image.img_shape("resources/Blacksun.jpg")
-    print("Image size: ",resolution[0],"x",resolution[1])
-    pixel = load_image.img_pixel("resources/Blacksun.jpg")
-    
+    print("Image size: ",resolution[0],"x",resolution[1]) #Verifico el tamaño de mi imagen
+    pixel = load_image.img_pixel("resources/Blacksun.jpg") #Cargo los pixeles punto por punto
+    load_image.filter("average", pixel, resolution)
+    aver_value = int(pixel[0,0][1])+ int(pixel[0,0][1])
+    a = pixel[0,0][0]
+    print(aver_value)
 
 if __name__ == "__main__":
     main()
