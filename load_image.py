856fc8cb9c32840af96c9fd297ef3492339c16c8
thobyas <thobyascastano@gmail.com>
2023-04-24 21:45:49 -0500

    Se soluciono el problema que ocacionaba el uso de la misma memoria para crear la matriz, pero aun no se crea la matrriz adecuada para mostrarse

--- load_image.py
+++ load_image.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy
+import numpy as np
 
 def img_shape(pc_path):
 #LOAD AN IMAGE USING 'IMREAD'
@@ -15,22 +15,25 @@ def img_pixel(pc_path):
     #print(img)
     return img
 
-def filter(type, pixel,size):  
+def filter(type, pixel,size): 
     filter_img = []
-    img_out = []
+    matrix = np.zeros((3, 3))
+    img_out = [[0 for j in range(3)] for i in range(3)]
     if type == "average":
-        for i in range(2):
-            for j in range(1):
-                #aver_value = (int(pixel[i,j][0]) + int(pixel[i,j][1]) + int(pixel[i,j][2]))/3
+        for i in range(3):
+            for j in range(3):
+                aver_value = (int(pixel[i,j][0]) + int(pixel[i,j][1]) + int(pixel[i,j][2]))/3
                 aver_value = 3
-                filter_img.append([aver_value, aver_value, aver_value])
-            img_out.append(filter_img)
+                img_out[i][j] = [aver_value, aver_value, aver_value]
+                datos = [[[1, 2, 3], [1, 2, 3]],[[4, 5, 6], [7, 8, 9]]]
+                matrix = np.array(datos)
+            #img_out.append(filter_img,0)
         else:
             pass
             #cv2.imshow('image',pixel)
             #cv2.waitKey(0)
-    # cv2.imshow('hola',img_out)
-    # cv2.waitKey(0)
-    print(img_out)
+    #cv2.imshow('hola',img_out)
+    #cv2.waitKey(0)
+    print(matrix)
 
 
